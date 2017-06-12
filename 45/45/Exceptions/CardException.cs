fa7d1b0c9206647cb4921d8b6072a08b98a34c14
DynamicDuo <DynamicDuo@DYNAMICDUO-PC>
2017-06-12 18:13:33 +0100

    card exception

---
+++ 45/45/Exceptions/CardException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _45.Exceptions
+{
+    class CardException : Exception
+    {
+        public CardException()
+            : base()
+        {
+           
+        }
+
+        public CardException(string message) 
+            : base(message)
+        {
+        }
+
+        public CardException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
--- 45/45/Model/Player.cs
+++ 45/45/Model/Player.cs
@@ -1,9 +1,6 @@
 ﻿using _45.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _45.Model
 {
